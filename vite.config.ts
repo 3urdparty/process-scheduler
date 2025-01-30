4d2d91af1db799055f686421636b29bb78f76554
3urdparty <3urdparty@gmail.com>
2025-01-30 16:59:49 +0800

    Changed base

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/grape-process-scheduler/",
-  plugins: [
-    vue(),
-  ],
+  base: '/process-scheduler/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
